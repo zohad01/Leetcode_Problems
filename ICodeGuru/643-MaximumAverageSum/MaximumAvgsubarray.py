6542f44b1a375f259bbf199c15a5ab3312210e4f
zohad01 <zohadijaz786@email.com>
2024-11-26 22:32:23 +0500

    Solution Added

---
+++ ICodeGuru/643-MaximumAverageSum/MaximumAvgsubarray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        curr_sum = sum(nums[:k])
+        max_sum = curr_sum
+        for i in range(k, len(nums)):
+            curr_sum += nums[i]
+            curr_sum -= nums[i-k]
+            max_sum = max(curr_sum, max_sum)
+        return max_sum/k
