b6051e08c8b8b69e0ee74b2fb8f038a63bdd828f
zohad01 <zohadijaz786@gmail.com>
2024-12-26 09:06:34 +0500

    Solution Added

---
+++ ICodeGuru/881-BoatsToSavePeople/BoatsToSavePeople.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        left = 0
+        right = len(people) - 1
+        boat = 0
+        people.sort()
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
+            boat += 1
+        return boat
+          
