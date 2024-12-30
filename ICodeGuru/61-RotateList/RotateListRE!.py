a1fba298fd1de19917c9a9559d8f448fba3e2a34
zohad01 <zohadijaz786@gmail.com>
2024-12-30 12:08:48 +0500

    Solution Added

---
+++ ICodeGuru/61-RotateList/RotateListRE!.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+       arr = []
+        cur = head
+        while cur:
+            arr.append(cur.val)
+            cur = cur.next
+
+        k = k % len(arr)
+        if k == 0:
+            return head
+        
+        arr = arr[-k:] + arr[:-k]
+
+        dummy = ListNode()
+        temp = dummy
+        
+        for val in arr:
+            temp.next = ListNode(val)
+            temp = temp.next
+        return dummy.next
