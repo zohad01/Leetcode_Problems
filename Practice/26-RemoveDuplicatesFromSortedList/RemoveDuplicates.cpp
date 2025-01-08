4866a7fa20b6f042b1c8ea06d6a5c23d1d99e335
zohad01 <zohadijaz786@gmail.com>
2025-01-08 12:52:36 +0500

    Solution Added

---
+++ Practice/26-RemoveDuplicatesFromSortedList/RemoveDuplicates.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l = 0;
+        if(nums.empty()){
+            return 0;
+        }
+        for(int r=1; r < nums.size(); r++){
+            if(nums[l] != nums[r]){
+            l++;
+            nums[l] = nums[r];
+            }
+        }
+        return l + 1;
+    }
+};
