3c9a1feaafa5dc864208d54409375cdd99a252a4
zohad01 <zohadijaz786@gmail.com>
2025-01-09 16:58:56 +0500

    Solution Added

---
+++ Practice/75-SortColors/SortColor.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int current = 0;
+
+        while (current <= right) {
+            if (nums[current] == 0) {
+                swap(nums[current], nums[left]);
+                left++;
+                current++;
+            } else if (nums[current] == 2) {
+                swap(nums[current], nums[right]);
+                right--;
+            } else {
+                current++;
+            }
+        }
+    }
+};
+};
