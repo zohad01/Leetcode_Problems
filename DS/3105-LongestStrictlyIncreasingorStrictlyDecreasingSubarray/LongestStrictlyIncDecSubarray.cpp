88ca2957f865db9a426187117b37750830e5bec9
zohad01 <zohadijaz786@gmail.com>
2025-02-03 08:23:10 +0500

    Solution Added

---
+++ DS/3105-LongestStrictlyIncreasingorStrictlyDecreasingSubarray/LongestStrictlyIncDecSubarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums){
+        
+        int inc = 1, dec = 1, maxLen = 1;
+        for(int i = 0; i < nums.size() - 1; i++){
+            if(nums[i] < nums[i+1]){
+                inc++;
+                dec = 1;
+            }else if(nums[i] > nums[i + 1]){
+                dec ++;
+                inc = 1;
+            }else{
+                inc = 1;
+                dec = 1;
+            }
+            maxLen = max(maxLen , max(inc, dec));
+        }
+        return maxLen;
+    }
+};
